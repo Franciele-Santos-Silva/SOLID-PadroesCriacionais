ac9154410a06e97678cee56765acd9258559d252
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-03-15 14:51:51 -0300

    adicionar ConfiguracaoSistema singleton

---
+++ TDE/Singleton/ConfiguracaoSistema.cs
@@ -0,0 +1,23 @@
+public class ConfiguracaoSistema{
+
+    private static ConfiguracaoSistema instancia;
+
+    public string NomeSistema {get; private set;}
+    public string Versao {get; private set;}
+
+    private ConfiguracaoSistema(){
+
+        NomeSistema = "Sistema de Notificações";
+        Versao = "1.0";
+    }
+
+    public static ConfiguracaoSistema GetInstancia(){
+        
+        if(instancia == null)
+        {
+            instancia = new ConfiguracaoSistema();
+        }
+        return instancia
+    }
+
+}
