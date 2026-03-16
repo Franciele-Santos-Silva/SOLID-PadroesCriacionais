f22a5d4979e5735e42361891a0d76a65125a2c64
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-03-16 15:46:41 -0300

    feat(singleton): adicionar ConfiguracaoSistema

--- TDE/Singleton/ConfiguracaoSistema.cs
+++ TDE/Singleton/ConfiguracaoSistema.cs
@@ -1,27 +1,25 @@
-
-namespace Singleton{
-
-    public class ConfiguracaoSistema{
-
+namespace Singleton
+{
+    public class ConfiguracaoSistema
+    {
         private static ConfiguracaoSistema instancia;
 
-        public string NomeSistema {get; private set;}
-        public string Versao {get; private set;}
+        public string NomeSistema { get; private set; }
+        public string Versao { get; private set; }
 
-        private ConfiguracaoSistema(){
-
+        private ConfiguracaoSistema()
+        {
             NomeSistema = "Sistema de Notificações";
             Versao = "1.0";
         }
 
-        public static ConfiguracaoSistema GetInstancia(){
-            
-            if(instancia == null)
+        public static ConfiguracaoSistema GetInstancia()
+        {
+            if (instancia == null)
             {
                 instancia = new ConfiguracaoSistema();
             }
             return instancia;
         }
-
     }
 }
